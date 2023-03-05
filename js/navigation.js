8a1508bcd995ef811fcd0809e609a5c4f8112d85
jannatul-akhi <jannatulferdous3608@gmail.com>
2023-03-06 00:47:34 +0600

    all commit

---
+++ js/navigation.js
@@ -0,0 +1,43 @@
+document.getElementById('color-change1').addEventListener('click', function(){
+    document.getElementById('color-change1').style.color = 'wheat';
+    document.getElementById('color-change1').style.fontWeight = 800;
+    document.getElementById('color-change2').style.color = 'white';
+    document.getElementById('color-change3').style.color = 'white';
+    document.getElementById('color-change4').style.color = 'white';
+    document.getElementById('color-change5').style.color = 'white';
+})
+document.getElementById('color-change2').addEventListener('click', function(){
+    document.getElementById('color-change2').style.color = 'wheat';
+    document.getElementById('color-change2').style.fontWeight = 800;
+    document.getElementById('color-change1').style.color = 'white';
+    document.getElementById('color-change3').style.color = 'white';
+    document.getElementById('color-change4').style.color = 'white';
+    document.getElementById('color-change5').style.color = 'white';
+})
+document.getElementById('color-change3').addEventListener('click', function(){
+    document.getElementById('color-change3').style.color = 'wheat';
+    document.getElementById('color-change3').style.fontWeight = 800;
+    document.getElementById('color-change1').style.color = 'white';
+    document.getElementById('color-change2').style.color = 'white';
+    document.getElementById('color-change4').style.color = 'white';
+    document.getElementById('color-change5').style.color = 'white';
+})
+document.getElementById('color-change4').addEventListener('click', function(){
+    document.getElementById('color-change4').style.color = 'wheat';
+    document.getElementById('color-change4').style.fontWeight = 800;
+    document.getElementById('color-change2').style.color = 'white';
+    document.getElementById('color-change3').style.color = 'white';
+    document.getElementById('color-change1').style.color = 'white';
+    document.getElementById('color-change5').style.color = 'white';
+})
+
+document.getElementById('color-change5').addEventListener('click', function(){
+    document.getElementById('color-change5').style.color = 'wheat';
+    document.getElementById('color-change5').style.fontWeight = 800;
+    document.getElementById('color-change1').style.color = 'white';
+    document.getElementById('color-change2').style.color = 'white';
+    document.getElementById('color-change3').style.color = 'white';
+    document.getElementById('color-change4').style.color = 'white';
+})
+
+
